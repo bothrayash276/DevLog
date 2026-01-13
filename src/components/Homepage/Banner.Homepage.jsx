9e593450ee9b09d4d6a62a698f80a1778bfab76d
bothrayash276 <bothrayash276@gmail.com>
2026-01-13 23:15:00 +0530

    add comment to navbar and homepage and make the code a bit cleaner and easier to read

--- src/components/Homepage/Banner.Homepage.jsx
+++ src/components/Homepage/Banner.Homepage.jsx
@@ -4,9 +4,18 @@ import "../CustomFonts.css";
 const Banner_Homepage = () => {
   return (
     <>
-      <div className="flex h-[80vh]">
-        <div className="flex-1"></div>
+    {/*Container for two sides of banner*/}
+      <div className="flex h-[80vh]">       
+
+        {/* Container of left side*/ }
+        <div className="flex-1">
+            
+        </div>
+
+        {/* Container for right side of the banner*/}      
         <div className="flex flex-col items-center flex-1 p-4">
+
+            {/* Header of the window of the journal */}
           <div className="bg-slate-800 h-13 w-8/10 flex items-center p-2 rounded-t-2xl border border-slate-700">
             <span className="h-3 w-3 rounded-full bg-red-500 ml-3 mr-1"></span>
             <span className="h-3 w-3 rounded-full bg-yellow-300 mx-1"></span>
@@ -18,16 +27,28 @@ const Banner_Homepage = () => {
             </div>
             <img src="asset/images/settings.svg" className="mr-3" />
           </div>
+
+          {/* The acutal window of the journal */}
           <div className="bg-slate-800 h-8/10 w-8/10 flex flex-col p-2 px-4 gap-5 rounded-b-2xl border border-slate-700">
+
+          {/* The Timing */}
             <span className="text-slate-500 font-[JetBrains] mt-3 ">
               # 29 Feb : Created a Sum Code in C++
             </span>
+
+            {/* The journal entry */}
             <span className="text-stone-100 font-[JetBrains]">
               I am currently at the library. This is the winter season and my
               hands are freezing
             </span>
+
+            {/* Container for the code inside the journal */}
             <div className="flex justify-center flex-1 pt-2">
+
+                {/* Code inside the journal */}
               <div className="container bg-[#0F172A] w-8/10 h-8/10 rounded-lg p-2">
+
+              {/* Shows #include <iostream>; */}
                 <p className="mb-5">
                   <span className="text-pink-500 font-[JetBrains] mr-2">
                     #include
@@ -35,6 +56,8 @@ const Banner_Homepage = () => {
                   <span className="text-blue-400">&#60;iostream&#62;</span>
                   <span className="text-white font-[JetBrains]">;</span>
                 </p>
+
+                {/* Shows int main() { */}
                 <p>
                   <span className="text-pink-500 font-[JetBrains] mr-2">
                     int
@@ -46,6 +69,8 @@ const Banner_Homepage = () => {
                     &#123;
                   </span>
                 </p>
+
+                {/* Shows declaration and initialization of a and b */}
                 <p>
                   <span className="text-pink-500 font-[JetBrains] ml-10 mr-2">
                     int
@@ -56,6 +81,8 @@ const Banner_Homepage = () => {
                   </span>
                   <span className="text-white font-[JetBrains]">;</span>
                 </p>
+
+                {/* Shows the cout function */}
                 <p>
                   <span className="text-green-400 font-[JetBrains] ml-10 mr-2">
                     cout
@@ -68,6 +95,11 @@ const Banner_Homepage = () => {
                     b
                   </span>
                   <span className="text-white font-[JetBrains]">;</span>
+                </p>
+
+                {/* Ending the of the main() function */}
+                <p>
+                    <span className="text-yellow-400 font-[JetBrains]">&#125;</span>
                 </p>
               </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,28 @@ import "./CustomFonts.css";
 const Navbar = () => {
   return (
     <>
+    {/* Container of the Navbar */}
       <div className="container flex bg-[#0F172A] h-17 items-center px-4 justify-evenly border-b-2 border-slate-800">
+
+        {/* Container that stores Name and Logo of the Website in Navbar */}
         <div className="flex flex-2 items-center gap-4">
             <div className="bg-[#135BEC] p-2 rounded-lg">
           <img src="asset/images/logo.svg" className="w-6" />
             </div>
           <span className="text-white text-xl font-[JetBrainsBold]">DevLog</span>
         </div>
+
+        {/* Container that stores Link Routes to Home and MyLog in Navbar */}
         <div className="container flex-1 flex justify-center gap-10">
             <Link to="/" className="text-white text-[14px] font-[JetBrains]">Home</Link>
             <Link to="/mylogs" className="text-white text-[14px] font-[JetBrains]">MyLogs</Link>
         </div>
+
+        {/* Container that stores the Create a Log Button in Navbar */}
         <div className="container flex-10 flex justify-end">
             <button className="text-white font-[JetBrainsBold] text-[14px] bg-[#135BEC] px-6 py-[6px] rounded-lg">Start Your Log</button>
         </div>
+        
       </div>
     </>
   );
