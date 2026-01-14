42584fe77cc4762fd46024421e37a17e8a9d061f
bothrayash276 <bothrayash276@gmail.com>
2026-01-14 22:17:08 +0530

    add the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Navbar from './components/Navbar'
 import HomeMainPage from './components/HomeMainPage'
 import MyLogs from './components/MyLogs'
 import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path="/" element={<><Navbar/><HomeMainPage/></>} />
-      <Route path="/mylogs" element={<><Navbar/><MyLogs/></>} />
+      <Route path="/" element={<><Navbar/><HomeMainPage/><Footer/></>} />
+      <Route path="/mylogs" element={<><Navbar/><MyLogs/><Footer/></>} />
     </Routes>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      {/* Black Strip Container - All content is in this */}
+      <div className="bg-black h-[20vh] flex justify-center items-center gap-15 mt-12">
+        {/* Contains the Logo and name of the website with website's description */}
+        <div className="flex flex-col w-[20vw] gap-4">
+          {/* Contains Image and Website Name */}
+          <div className="container flex gap-4 items-center">
+            <img src="asset/images/devlog.svg" className="w-9" alt="" />
+            <span className="text-white font-[JetBrainsBold] text-xl">
+              DevLog
+            </span>
+          </div>
+          {/* Description of the website */}
+          <span className="text-[#94A3B8] text-xs font-[JetBrains] ">
+            A Daily Journal Website for Programmers who can write their all day
+            longs struggles.
+          </span>
+        </div>
+        {/* Contains the Links and Redirects to my social handles */}
+        <div className="flex flex-col gap-4 h-1/1 py-6 ">
+          {/* Heading */}
+          <span className="text-xs font-[JetBrains] text-[#94A3B8]">
+            HANDLES
+          </span>
+          {/* GitHub */}
+          <div className="flex gap-2">
+            <img src="asset/images/github.svg" alt="" />
+            <a href="https://github.com/bothrayash276/" target="_blank" className="text-white font-[JetBrains] text-xs hover:underline underline-offset-4">
+              GitHub
+            </a>
+          </div>
+          {/* LinkedIn */}
+          <div className="flex gap-2">
+            <img src="asset/images/linkedin.svg" alt="" />
+            <a href="https://www.linkedin.com/in/yashbothra1" target="_blank" className="text-white font-[JetBrains] text-xs hover:underline underline-offset-4">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
