4bf9b69eeac34d1cbbbd0c4e503ebec9a032d2ad
bothrayash276 <bothrayash276@gmail.com>
2026-02-07 10:13:47 +0530

    making app production ready p1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import HomeMainPage from './components/HomeMainPage'
 import MyLogs from './components/MyLogs'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
-import Dev from './components/dev'
 import EntryLog from './components/TextWindow/EntryLog'
 import EditWindow_MyLogs from './components/MyLog/EditWindow.MyLogs'
 
@@ -17,7 +16,6 @@ function App() {
       <Route path="/mylogs" element={<><Navbar/><MyLogs/><Footer/></>} />
       <Route path="/mylogs/newlog" element={<><EntryLog/></>} />
       <Route path="/mylogs/editlog/:id" element={<><EditWindow_MyLogs/></>} />
-      <Route path="/dev" element={<><Dev/></>} />
     </Routes>
     </>
   )
